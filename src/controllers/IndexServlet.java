8f7d25286e068cffdd0c9766a07debbf83dbe6a6
shuhei.goda <shuhei.coron93@outlook.jp>
2022-03-12 11:24:00 +0900

    バリデーションを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -27,20 +27,36 @@ public class IndexServlet extends HttpServlet {
      */
     public IndexServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        }catch(NumberFormatException e) {}
+
+
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Message> messages = em.createNamedQuery("getAllMessages",Message.class).getResultList();
+
+        List<Message> messages = em.createNamedQuery("getAllMessages",Message.class).setFirstResult(15*(page -1)).setMaxResults(15).getResultList();
+
+        long messages_count = (long)em.createNamedQuery("getMessagesCount", Long.class)
+                .getSingleResult();
 
         em.close();
 
         request.setAttribute("messages", messages);
+        request.setAttribute("messages_count", messages_count);     // 全件数
+        request.setAttribute("page", page);
+
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
         rd.forward(request, response);
